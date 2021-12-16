2c7b02e20bcc42df286cd602db6fd3161eb9111f
netcan <1469709759@qq.com>
2021-12-16 22:08:26 +0800

    promise final_suspend noexcept

--- include/asyncio/concept/promise.h
+++ include/asyncio/concept/promise.h
@@ -10,7 +10,7 @@ template<typename Promise>
 concept promise = requires (Promise p) {
     p.get_return_object();
     { p.initial_suspend() } -> Awaitable;
-    { p.final_suspend() } -> Awaitable;
+    { p.final_suspend() } noexcept -> Awaitable;
     p.unhandled_exception();
     requires (requires(int v) { p.return_value(v); } ||
               requires        { p.return_void();   });
