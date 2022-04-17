1481361b11846a9a495a1f67e1b513eca9df02df
netcan <1469709759@qq.com>
2022-04-17 14:30:09 +0800

    fix echo_client bug

--- test/st/echo_client.cpp
+++ test/st/echo_client.cpp
@@ -3,16 +3,18 @@
 //
 #include <asyncio/open_connection.h>
 #include <asyncio/runner.h>
+#include <asyncio/wait_for.h>
 using asyncio::Task;
 using asyncio::Stream;
+using namespace std::chrono;
 
 Task<> tcp_echo_client(std::string_view message) {
     auto stream = co_await asyncio::open_connection("127.0.0.1", 8888);
 
     fmt::print("Send: '{}'\n", message);
-    co_await stream.write(Stream::Buffer(message.begin(), message.end()));
+    co_await stream.write(Stream::Buffer(message.begin(), message.end() + 1 /* plus '\0' */));
 
-    auto data = co_await stream.read(100);
+    auto data = co_await asyncio::wait_for(stream.read(100), 300ms);
     fmt::print("Received: '{}'\n", data.data());
 
     fmt::print("Close the connection\n");
